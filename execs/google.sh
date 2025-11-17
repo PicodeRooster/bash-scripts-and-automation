90d919eeba7e9c7c58292d90fbeffa73f769ecf3
PicodeRooster <colettemoreau30@gmail.com>
2025-11-17 00:58:24 -0400

    updated google.sh

--- execs/google.sh
+++ execs/google.sh
@@ -1,3 +1,3 @@
 query="$1"
-url="http://www.google.com/search?q=${query}"
-xdg-open "$url"
+url="http://www.google.com/search?q=${query// /+}" 
+xdg-open "$url" >/dev/null 2>&1 & disown
--- execs/latestimg.sh
+++ execs/latestimg.sh
@@ -2,8 +2,8 @@ cat << description
 Copy latest screenshot from PC to destination folder.
 description
 
-read -rp "Destination dir: " dir
+read -rp "Destination dir: " dir 
 
 cd ~/Pictures/Screenshots
 newest=$(ls -Art | tail -n 1)
-cp "$newest" "$dir" || { echo "No directory found."; exit 1; }
+cp "$newest" "$dir" || echo "No directory found."; exit 1;
